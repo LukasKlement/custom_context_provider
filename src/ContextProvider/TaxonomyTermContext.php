a5d99f90e9317b8a07e3946aa99265f89979a4c7
Lukas Klement <LukasKlement@users.noreply.github.com>
2018-02-18 15:43:02 +0100

    Initial commit
    Module works with Drupal 8.4.4, Block Visibility Groups 8.x-1.1 and https://github.com/md-systems/vocabulary_condition

---
+++ src/ContextProvider/TaxonomyTermContext.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Drupal\custom_context_provider\ContextProvider;
+
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Plugin\Context\Context;
+use Drupal\Core\Plugin\Context\ContextDefinition;
+use Drupal\Core\Plugin\Context\ContextProviderInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Sets the current taxonomy term as a context on taxonomy term routes.
+ */
+class TaxonomyTermContext implements ContextProviderInterface
+{
+
+    use StringTranslationTrait;
+
+    /**
+     * The route match object.
+     *
+     * @var \Drupal\Core\Routing\RouteMatchInterface
+     */
+    protected $routeMatch;
+
+    /**
+     * Constructs a new object.
+     *
+     * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+     *   The route match object.
+     */
+    public function __construct(RouteMatchInterface $route_match)
+    {
+        $this->routeMatch = $route_match;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRuntimeContexts(array $unqualified_context_ids)
+    {
+        $result = [];
+        $context_definition = new ContextDefinition('entity:taxonomy_term', $this->t('Taxonomy term from URL'));
+        $value = null;
+        if (($route_object = $this->routeMatch->getRouteObject())
+            && ($route_contexts = $route_object->getOption('parameters'))
+            && isset($route_contexts['taxonomy_term'])) {
+            if ($term = $this->routeMatch->getParameter('taxonomy_term')) {
+                $value = $term;
+            }
+        }
+
+        $cacheability = new CacheableMetadata();
+        $cacheability->setCacheContexts(['route']);
+
+        $context = new Context($context_definition, $value);
+        $context->addCacheableDependency($cacheability);
+        $result['taxonomy_term'] = $context;
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAvailableContexts()
+    {
+        $context = new Context(new ContextDefinition('entity:taxonomy_term', $this->t('Taxonomy term from URL')));
+        return ['taxonomy_term' => $context];
+    }
+
+}
